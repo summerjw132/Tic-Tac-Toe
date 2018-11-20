3b82c2f7be554dec6b31f13e435bbd568d9dd467
Jesus' Blood <jesusblood@johns-macbook-pro.local>
2018-11-19 19:03:24 -0600

    Initial Commit

---
+++ CPlusPlusProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPlusPlusProject
+//
+//  Created by Jesus' Blood on 11/19/18.
+//  Copyright © 2018 Jesus' Blood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
